99f1101dc99611c7a04d00c40b6a587d84e256cf
hafidzpsb <randombs51102@gmail.com>
2023-12-11 18:05:55 +0700

    database user profile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,25 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('id_user');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('isAdmin');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'id_user' => 1,
+            'username'=>'Admin',
+            'email'=>'admin@gmail.com',
+            'isAdmin'=>true,
+            'password'=> bcrypt('admin'),
+        ]);
+        Profile::create([
+            'id_user'=> 1,
+            'nama_depan'=>'John',
+            'nama_belakang'=>'Doe',
+            'jenis_kelamin'=>'Pria',
+            'tanggal_lahir'=>null,
+            'alamat'=>'Bandung',
+        ]);
     }
 }
